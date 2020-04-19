1653589c794638db6aec7062afbea95038855595
timdhughes <tdhughe@gmail.com>
2020-04-19 12:12:51 -0700

    initial servo_motor test

---
+++ tests/servo_motor/servo_motor.ino
@@ -0,0 +1,35 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  // SG90 datasheet states that the minimum pulse width is 1000 us and maxmimum is 2000 us
+  // After some experimentation, applying a scale factor of 3/4 gave the best results.
+  myservo.attach(6, 750, 1500);  
+}
+
+void loop() {
+    //myservo.write(90);              // tell servo to go to position in variable 'pos'
+    //return; 
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
